1591e7590611aa7dae54b5b72fd65e1cb8a12e93
Micheal Joel Chukwunekwu <michealngini@gmail.com>
2023-03-17 18:26:15 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main - print a random number
+ * main - assign a random number
  *
  * Return: 0
  */
@@ -12,11 +12,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	
 	return (0);
 }
