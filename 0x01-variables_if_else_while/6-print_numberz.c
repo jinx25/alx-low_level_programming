4f679cb3935587d87a32e8b36ca9b1a71c2b1bc1
Micheal Joel Chukwunekwu <michealngini@gmail.com>
2023-03-17 19:53:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+       	for (a = o; a < 10; a++)
+		putchar(a + '0');
+	putchar("\n");
+	return (0);
+
+}
