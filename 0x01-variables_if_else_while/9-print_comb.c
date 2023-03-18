afca5c5a7eda89d429beffe2e5fcf868afe72692
Micheal Joel Chukwunekwu <michealngini@gmail.com>
2023-03-17 20:30:08 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,15 @@ int main(void)
 {
 	int i;
 	for (i = 0; i < 10; i++)
-		putchar(i + '0');
-	putchar(',');
-	putchar(' ');
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	return (0);
+}
